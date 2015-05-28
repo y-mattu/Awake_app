276e405dac7856a5b96d828e93728538ab4eef84
Yuta <winningman@icloud.com>
2015-05-28 14:46:57 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwakeApp::Application.config.secret_key_base = '71c9ad5b382d44c6a90f6f2c1e783b77b209763444af0af822975e70f7f3ec8c6f62901e4257d1bc6635fc87dc4e77f23b9031b90b81e307671478ff9cafbeb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AwakeApp::Application.config.session_store :cookie_store, key: '_Awake_app_session'
